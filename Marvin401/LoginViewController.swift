f122742db29ce42d5b492c88b2aa5311202b2d1f
Mazen Azar <mjazar12@gmail.com>
2017-02-11 15:26:56 -0800

    login view controller

--- Marvin401/LoginViewController.swift
+++ Marvin401/LoginViewController.swift
@@ -1,29 +1,26 @@
 //
-//  ViewController.swift
+//  LoginViewController.swift
 //  Marvin401
 //
-//  Created by Mazen Azar on 1/26/17.
+//  Created by Mazen Azar on 2/11/17.
 //  Copyright © 2017 Mazen Azar. All rights reserved.
 //
 
+
 import UIKit
 
-class ViewController: UIViewController {
-
-    @IBOutlet weak var FirstNameTextField: UITextField!
-    @IBOutlet weak var LastNameTextfield: UITextField!
+class LoginViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-       
+        
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
-
