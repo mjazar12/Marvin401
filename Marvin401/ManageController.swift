b40b2b9614a3a61dfd177743c98caa1d598c9219
Courtney-Lynn Presto <owner@10.123.177.71>
2017-03-24 15:03:50 -0700

    fully functioning tabs (design way off lol)

---
+++ Marvin401/ManageController.swift
@@ -0,0 +1,55 @@
+//
+//  ManageController.swift
+//  Marvin401
+//
+//  Created by Courtney-Lynn Presto on 3/24/17.
+//  Copyright © 2017 Mazen Azar. All rights reserved.
+//
+
+import UIKit
+
+class ManageController: UIViewController {
+    @IBOutlet weak var understandContainer: UIView!
+    @IBOutlet weak var findContainer: UIView!
+    @IBOutlet weak var manageContainer: UIView!
+    @IBOutlet weak var connectContainer: UIView!
+    
+    @IBOutlet weak var segmentController: UISegmentedControl!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func showComponent(sender: UISegmentedControl) {
+        if segmentController.selectedSegmentIndex == 0 {
+            UIView.animate(withDuration: 0.5, animations: {
+                self.understandContainer.alpha = 1
+                self.findContainer.alpha = 0
+                self.manageContainer.alpha = 0
+                self.connectContainer.alpha=0
+            })
+        } else if segmentController.selectedSegmentIndex == 1 {
+            UIView.animate(withDuration: 0.5, animations: {
+                self.understandContainer.alpha = 0
+                self.findContainer.alpha = 1
+                self.manageContainer.alpha = 0
+                self.connectContainer.alpha=0
+            })
+        }  else if segmentController.selectedSegmentIndex == 2 {
+            UIView.animate(withDuration: 0.5, animations: {
+                self.understandContainer.alpha = 0
+                self.findContainer.alpha = 0
+                self.manageContainer.alpha = 1
+                self.connectContainer.alpha=0
+            })
+        } else {
+            UIView.animate(withDuration: 0.5, animations: {
+                self.understandContainer.alpha = 0
+                self.findContainer.alpha = 0
+                self.manageContainer.alpha = 0
+                self.connectContainer.alpha = 1
+            })
+        }
+        
+    }
+}
